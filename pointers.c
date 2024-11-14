c9ca1947e9ddb73c02fc0b8e99aba34fa931c0c1
achilles-000 <stefanpopovic976@gmail.com>
2024-11-14 09:31:31 -0500

    Update pointers.c

--- pointers.c
+++ pointers.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 
+void function(int *x){
+    //Derefrences to access the pointer at that memory location
+    *x = 100;
+
+
+}
+
 int main(){
 
-int abc = 1;
+    int num = 1;
 
-printf("%p", &abc);
+    printf("\nOriginal value: %d", num);
 
+    //Pass the address
+    function(&num);
 
+    printf("\nAfter: %d", num);
+
+    return 0;
 
 }
