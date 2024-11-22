e98938df4d758b189ac2d23b529f309594a07370
achilles-000 <stefanpopovic976@gmail.com>
2024-11-22 14:51:37 -0500

    example code

---
+++ struct_example.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+
+struct Features{
+    int width;
+    char symbol;
+    int height;
+};
+
+int main(){
+
+//Creates the variable features1
+struct Features features1;
+
+features1.width = 1;
+features1.symbol = 'x';
+
+printf("\n%d", features1.width );
+printf("\n%c", features1.symbol );
+
+
+
+
+    return 0;
+}
